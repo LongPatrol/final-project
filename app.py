8976d27a9c3295753ab675fe3286b5336bdd0b6b
MSabo <72773063+LongPatrol@users.noreply.github.com>
2021-04-24 13:55:46 -0400

    Importing the model file at the beginning of the file and making a route for running the parameters

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ from flask import Flask
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
 
+import app_model
 
 # 3. Define what to do when a user hits the index route
 @app.route("/")
@@ -36,6 +37,16 @@ def about():
     print("Server received request for 'About' page...")
     return "Welcome to my 'About' page!"
 
+@app.route("/results")
+def about():
+    #10 parameters for the 10 books we're using instead of the username and password
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+   #Do prediction here: take values from parameters; 
+    new_book_data = [[0,0,0,0,0,0,0,0,0,1]]
+    predicted_class = Pickled_book_model.predict(new_book_data)
+    return "Welcome to my 'About' page!"
 
 if __name__ == "__main__":
     app.run(debug=True)
