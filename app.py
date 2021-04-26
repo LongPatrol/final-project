118ea650ca1c79a82dd56966f3c42f7dd02726ac
MSabo <72773063+LongPatrol@users.noreply.github.com>
2021-04-26 18:31:08 -0400

    Commenting out parameter code

--- app.py
+++ app.py
@@ -40,13 +40,20 @@ def about():
 @app.route("/results")
 def about():
     #10 parameters for the 10 books we're using instead of the username and password
-    username = request.args.get('username')
-    password = request.args.get('password')
+    #username = request.args.get('username')
+    #password = request.args.get('password')
 
    #Do prediction here: take values from parameters; 
-    new_book_data = [[0,0,0,0,0,0,0,0,0,1]]
-    predicted_class = Pickled_book_model.predict(new_book_data)
-    return "Welcome to my 'About' page!"
+    #new_book_data = [[0,0,0,0,0,0,0,0,0,1]]
+    #predicted_class = Pickled_book_model.predict(new_book_data)
+    
+    if predicted_class == 0:
+        return "Shoot! The user will not buy Bridget Jones's Diary"
+    elif predicted_class == 1:
+        return "The user will buy Bridget Jones's Diary. Yay!"
+    else:
+        return "Something went horribly wrong" 
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
