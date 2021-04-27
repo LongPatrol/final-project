492a3ee2f8e91670be5f8f547aea38beba5e09a7
MSabo <72773063+LongPatrol@users.noreply.github.com>
2021-04-26 21:05:14 -0400

    Model route

--- app.py
+++ app.py
@@ -37,23 +37,21 @@ def about():
     print("Server received request for 'About' page...")
     return "Welcome to my 'About' page!"
 
-#@app.route("/results")
-#def about():
-    #10 parameters for the 10 books we're using instead of the username and password
-    #username = request.args.get('username')
-    #password = request.args.get('password')
+#Making changes here
+@app.route("/results/<book_one>/<book_two>/<book_three>/<book_four>/<book_five>/<book_six>/<book_seven>/<book_eight>/<book_nine>/<book_ten>")
+def run_model(book_one, book_two, book_three, book_four, book_five, book_six, book_seven, book_eight, book_nine, book_ten):
 
-    #Do prediction here: take values from parameters; 
     new_book_data = [[book_one, book_two, book_three, book_four, book_five, book_six, book_seven, book_eight, book_nine, book_ten]]
     predicted_class = app_model.Pickled_book_model.predict(new_book_data)
     
-    # if predicted_class == 0:
-    #     return "Shoot! The user will not buy Bridget Jones's Diary"
-    # elif predicted_class == 1:
-    #     return "The user will buy Bridget Jones's Diary. Yay!"
-    # else:
-    #     return "Something went horribly wrong" 
-    # return "Results page placeholder"
+    if predicted_class == 0:
+        return "Shoot! The user will not buy Bridget Jones's Diary"
+    elif predicted_class == 1:
+        return "The user will buy Bridget Jones's Diary. Yay!"
+    else:
+        return "Something went horribly wrong" 
+    
+    return "Results page placeholder"
         
 
 if __name__ == "__main__":
