8557f66e68da2a325592766cf5e0156d0cf2bec8
MSabo <72773063+LongPatrol@users.noreply.github.com>
2021-04-26 20:01:30 -0400

    Working route for the model running.

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ import app_model
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def home():
-    return render_template("homeindex.html",)
+    return render_template("homeindex.html")
   # return "Welcome to my 'info' page!"
 
 # 4. Define what to do when a user hits the /about route
@@ -37,22 +37,24 @@ def about():
     print("Server received request for 'About' page...")
     return "Welcome to my 'About' page!"
 
-@app.route("/results")
-def about():
+#Making changes here
+@app.route("/results/<book_one>/<book_two>/<book_three>/<book_four>/<book_five>/<book_six>/<book_seven>/<book_eight>/<book_nine>/<book_ten>")
+def run_model(book_one, book_two, book_three, book_four, book_five, book_six, book_seven, book_eight, book_nine, book_ten):
+
     #10 parameters for the 10 books we're using instead of the username and password
     #username = request.args.get('username')
     #password = request.args.get('password')
 
-   #Do prediction here: take values from parameters; 
-    #new_book_data = [[0,0,0,0,0,0,0,0,0,1]]
-    #predicted_class = Pickled_book_model.predict(new_book_data)
+    #Do prediction here: take values from parameters; 
+    new_book_data = [[book_one, book_two, book_three, book_four, book_five, book_six, book_seven, book_eight, book_nine, book_ten]]
+    predicted_class = app_model.Pickled_book_model.predict(new_book_data)
     
-    # if predicted_class == 0:
-    #     return "Shoot! The user will not buy Bridget Jones's Diary"
-    # elif predicted_class == 1:
-    #     return "The user will buy Bridget Jones's Diary. Yay!"
-    # else:
-    #     return "Something went horribly wrong" 
+    if predicted_class == 0:
+        return "Shoot! The user will not buy Bridget Jones's Diary"
+    elif predicted_class == 1:
+        return "The user will buy Bridget Jones's Diary. Yay!"
+    else:
+        return "Something went horribly wrong" 
     
     return "Results page placeholder"
         
